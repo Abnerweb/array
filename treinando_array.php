0921c818dcacc8846291d17291a00b765bb4be80
Abner <abner.v.santos_2012@hotmail.com>
2020-03-12 15:31:49 -0300

    exercicio array

---
+++ treinando_array.php
@@ -0,0 +1,14 @@
+<?php
+
+$valor = $_GET['a'];
+
+$fruta = array("abacaxi","morango","manga","banana");
+echo $fruta[1];
+
+echo "<br>";
+
+$carro = array("corsa","gol","uno","kwid");
+echo $carro[$valor];
+
+
+?>
